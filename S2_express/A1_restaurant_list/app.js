00748065bbadb75fa79bd0a1a6b0b629eb2e7f3d
Rachel JTH <rachel98445111@gmail.com>
2022-07-31 23:28:25 +0800

    A1 restaurant list v1

---
+++ S2_express/A1_restaurant_list/app.js
@@ -0,0 +1,46 @@
+
+// initiate 
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+const port = 3000
+
+
+// setting the main template
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars') // to set the template engine, handlebars
+
+// setting external dataset
+const restList = require('./restaurant.json')
+
+// setting every routes to use the static file, 'public' which includes bootstrap and popper, etc.
+app.use(express.static('public'))
+
+
+// set the main page
+app.get('/restaurants', (req, res) => {
+    // res.send('test')
+    res.render('index', {rests:restList.results})
+})
+
+// show the details
+app.get('/restaurants/:id', (req, res) => {
+    const id = req.params.id
+    // console.log(req)
+    const detail = restList.results.find(rest => rest.id.toString() === id)
+    console.log(detail)
+    res.render('show', {detail: detail})
+})
+
+// req.query (of Express package)
+app.get('/search', (req,res) =>{
+    const keyword = req.query.keyword.toLowerCase()
+    const filteredList = restList.results.filter(rest => {return (rest.name.toLowerCase()).includes(keyword.toLowerCase())})
+    res.render('index', {rests: filteredList, keyword: keyword})
+})
+
+// start and listen on the Express Server
+app.listen(port, () => {
+    console.log(`Express is listened on localhost:${port}`)
+})
